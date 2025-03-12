3d5662a7040a3caf788d9ca1742cfe83fcb1ee52
Mathis Ballet <maballet@student.42lyon.fr>
2025-03-12 13:37:18 +0100

    (PIPEX-1) fix (makefile) : compile if libft modified

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,15 +6,32 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 13:05:34 by maballet          #+#    #+#             */
-/*   Updated: 2025/03/04 14:44:05 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2025/03/11 15:44:32 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include "../libft/inc/libft.h"
+# include "../libft/inc/libft.h"
 
-int	main(int argc, char **argv, char **envp);
+typedef struct s_data
+{
+	char	**cmd_1;
+	char	**cmd_2;
+	char	*path_1;
+	char	*path_2;
+	int		fd_in;
+	int		fd_out;
+	int		fd_pipe[2];
+	pid_t	pid1;
+	pid_t	pid2;
+	int		status;
+}				t_data;
+
+int		main(int argc, char **argv, char **envp);
+void	free_array(char **array);
+void	cmd_path_init(t_data *data, char **argv, char **envp);
+char	*find_executable(char *cmd, char **envp);
 
 #endif
--- libft/src/convert/ft_atoi.c
+++ libft/src/convert/ft_atoi.c
@@ -6,11 +6,21 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 18:09:17 by maballet          #+#    #+#             */
-/*   Updated: 2025/02/06 18:22:32 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2025/03/05 14:40:47 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void	atoi_extend(const char *nptr, int *i, int *sign)
+{
+	if (nptr[*i] == '-' || nptr[*i] == '+')
+	{
+		if (nptr[*i] == '-')
+			*sign = -1;
+		(*i)++;
+	}
+}
 
 long	ft_atoi(const char *nptr)
 {
@@ -21,16 +31,11 @@ long	ft_atoi(const char *nptr)
 	i = 0;
 	sign = 1;
 	result = 0;
-	if (nptr == NULL)
+	if (nptr[i] == '\0')
 		return (LONG_MAX);
 	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
-	if (nptr[i] == '+' || nptr[i] == '-')
-	{
-		if (nptr[i] == '-')
-			sign = -1;
-		i++;
-	}
+	atoi_extend(nptr, &i, &sign);
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		result = result * 10 + (nptr[i] - '0');
